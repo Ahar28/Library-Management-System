a75c33fa17d056852d21ca126fb8bb2f85afe6bd
ahar28 <ahar2899@gmail.com>
2023-05-27 17:38:57 -0300

    Revert "Fourth commit to check the push functionality"
    This reverts commit 0a689852b40a36354d90f2574b0c20939650bcda.

--- src/main/java/dal/ca/csci5308/Assignment1/Assignment1.java
+++ src/main/java/dal/ca/csci5308/Assignment1/Assignment1.java
@@ -3,8 +3,5 @@ package dal.ca.csci5308.Assignment1;
 public class Assignment1 {
     public static void main(String[] args) {
         System.out.println("Hello this is to check the push functionality");
-
-        System.out.println("Hello this is to check the push functionality 2");
-
     }
 }
